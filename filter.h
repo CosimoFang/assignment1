eeb4f64321d37a15765f1607fccd2c0bd9f78c90
Hongchao Fang <47910720+CosimoFang@users.noreply.github.com>
2023-09-28 19:47:09 -0700

    Add files via upload

---
+++ filter.h
@@ -0,0 +1,17 @@
+#ifndef __filter_h
+#define __filter_h
+
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+
+using namespace cv;
+
+int greyscale(cv::Mat& src, cv::Mat& dst);
+int blur5x5(cv::Mat& src, cv::Mat& dst);
+int sobelY3x3(cv::Mat& src, cv::Mat& dst);
+int sobelX3x3(cv::Mat& src, cv::Mat& dst);
+int magnitude(cv::Mat &sx, cv::Mat &sy, cv::Mat &dst);
+int blurQuantize(cv::Mat &src, cv::Mat &dst, int levels);
+int cartoon(cv::Mat &src, cv::Mat&dst, int levels, int magThreshold);
+int reverseColor(cv::Mat& src, cv::Mat& dst);
+#endif
